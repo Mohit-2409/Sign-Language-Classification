62eb7d530b19eb2dc26c44aaa09a7efe3830b926
mc5233 <mohit7052015@gmail.com>
2024-06-27 15:30:29 +0530

    Importing Libraries required

---
+++ Sign Language Classification.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+#using keras and models layers 
+#use matplot to determine visual graphs 
+#use seaborn 
+#tensorflow keras for imagegenerator 
+#MAIN - CNN will be used  dont forget that 
+import keras
